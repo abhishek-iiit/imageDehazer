bc307126322c58259f1d48390a1b50fafea50bb5
Abhishek Jaiswal <69477761+abhishek-iiit@users.noreply.github.com>
2023-05-03 16:39:46 +0530

    added changes

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import image_dehazer
 import os
 import numpy as np
 #importing the Libraries
-from gtts import gTTS           # used for converting text to speech
 import PIL                      # Python Imaging Library
-import gtts                     # Google's text to Speech API
 import pytesseract              # used for image to text conversion using OCR
 from tkinter import filedialog  # Used to provide GUI open/save feature
 from tkinter import *
 from PIL import Image,ImageTk   # used for handling image type file
-import pyperclip
 
 #defining the Window
 window = Tk()
@@ -25,22 +22,25 @@ lab.pack()
 #Defining the Labels
 message = Label(window, text="Image Dehazer" ,bg="#000000"  ,fg="#FFFF00"  ,width=50  ,height=3,font=('Helvetica', 35, 'italic bold '))
 message.place(x=60, y=10)
+message = Label(window, text="" ,bg="grey"  ,fg="black"  ,width=50  ,height=12, activebackground = "yellow" ,font=('Helvetica', 20 , ' bold ')) 
+message.place(x=150, y=170)
 
 def mainFunct():
-        window.filename =  filedialog.askopenfilename()
         # provides a dialog box for asking file to open and returns it's path
+        window.filename =  filedialog.askopenfilename()
         image_file= PIL.Image.open(window.filename)  
-        
         
         # Read input image
         image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
         
         # Remove haze from image
         HazeCorrectedImg, haze_map = image_dehazer.remove_haze(image, showHazeTransmissionMap=False)
-
-        # Save the output image to a file
-        output_path = "outputImages/result.png"
-        cv2.imwrite(output_path, HazeCorrectedImg)
+                
+        #Open video to save file
+        window.filename =  filedialog.asksaveasfilename()
+        HazeCorrectedImg.save(window.filename+ '.png')
+        HazeCorrectedImg = "Saved"
+        message.configure(text= HazeCorrectedImg)
 
 #Defining the buttons
 funct = Button(window, text="ImageDehazer", command=mainFunct  ,fg="red"  ,bg="white"  ,width=20  ,height=3, activebackground = "grey" ,font=('Helvetica', 15 , ' bold '))
